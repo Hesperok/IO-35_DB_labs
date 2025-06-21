58deb7443660b1fe809f77d2205a11b5f1083874
Hesperok <jaldak1983@gmail.com>
2025-06-21 19:17:35 +0300

    Update publish.sh

--- publish.sh
+++ publish.sh
@@ -15,7 +15,7 @@ cd docs/.vuepress/dist
 
 git init
 git add -A
-git commit -m 'publish'
+git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:boldak/<USERNAME>.github.io.git master
